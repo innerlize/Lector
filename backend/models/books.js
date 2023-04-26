b308cd78e0917e41375c9da6c5c6d8d48e9925ca
Alan <tiltedveggie@outlook.kr>
2023-04-25 22:07:22 -0300

    Migrate to mysql2 and add new variable for database port.

--- backend/models/books.js
+++ backend/models/books.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const db = mysql.createPool({
-	connectionLimit: 10,
 	host: process.env.HOST,
 	user: process.env.USER,
 	password: process.env.PASSWORD,
+	port: process.env.DB_PORT,
 	database: process.env.DATABASE
 });
 
